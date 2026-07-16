4fb683381923a5c193e899b2c25b7922db9c56b2
Avika <avikachaudhary0808@gmail.com>
2026-07-16 15:55:27 +0530

    Day 1: First Java Program

---
+++ 01_Java_Basics/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+-------------------------------------------------------
+Date    : 16-07-2026
+Topic   : First Java Program
+Author  : Avika Chaudhary
+Repository : JAVA-DSA
+-------------------------------------------------------
+*/
+
+public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("Hello Avika");
+        System.out.println("Welcome to my Java DSA Journey");
+        System.out.println("16 july 2026, thursday");
+    
+    }
+    
+}
