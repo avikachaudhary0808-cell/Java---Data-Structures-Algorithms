bf7fee782a12cedf14186eee9204f79bbfaa28c9
Avika <avikachaudhary0808@gmail.com>
2026-07-23 14:24:04 +0530

    Day 2: Variables and Data Types

---
+++ 01_Java_Basics/Variables.java
@@ -0,0 +1,19 @@
+public class Variables 
+{ 
+    public static void main (String [] args)
+    {
+        String name = "Avika";
+        int age = 20;
+        boolean isPlaced = true;
+        char $ = 'A';
+        double CGPA = 7.73;
+
+        System.out.println("Nmae:" + name);
+        System.out.println("Age:" + age);
+        System.out.println("Placed:" + isPlaced);
+        System.out.println("Grade:" + $);
+        System.out.println("CGPA:" + CGPA);
+    }
+}
+
+
