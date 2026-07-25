5c8602a634089c288ca580cf663d9a4132d8ee33
Avika <avikachaudhary0808@gmail.com>
2026-07-25 21:26:08 +0530

    Day 4 : If statemnets

---
+++ 01_Java_Basics/IfStatement.java
@@ -0,0 +1,14 @@
+public class IfStatement {
+    public static void main(String [] args) 
+    {
+        int cgpa = 6;
+        if (cgpa >= 7)
+        {
+            System.out.println("passed");
+        }
+        else 
+        {
+            System.out.println("fail");
+        }
+    }
+}
--- 01_Java_Basics/Operators.java
+++ 01_Java_Basics/Operators.java
@@ -3,7 +3,7 @@ public class Operators {
     public static void main(String[] args) {
         int a = 20;
         int b = 5;
-        
+        // arithmetic operations
         System.out.println("Addition :" +(a+b));
         System.out.println("Subtraction :" +(a-b));
         System.out.println("Multiplication :" +(a*b));
